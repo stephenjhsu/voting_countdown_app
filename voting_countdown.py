dd04be532689bfdd42aa1d8bf0df417e4954a56b
stephenjhsu <sjhsu@dons.usfca.edu>
2020-06-16 10:45:09 -0700

    registered type

--- voting_countdown.py
+++ voting_countdown.py
@@ -13,7 +13,7 @@ class voting(object):
 		self.time_till_election = rumps.MenuItem(title='placeholder')
 		self.app.menu = [self.time_till_election, 
 			None,
-			rumps.MenuItem("Am I registred to vote?"),
+			rumps.MenuItem("Am I registered to vote?"),
 			None, 
 			rumps.MenuItem("Register to Vote"),
 			None,
